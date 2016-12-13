dbe880d14e572ed5b15890db0397b13518aae374
Banu Prakash <banuprakashc@yahoo.co.in>
2016-12-13 16:29:28 +0530

    Update main.cfg.js

--- main.cfg.js
+++ main.cfg.js
@@ -1,9 +1,9 @@
 (function() {
-    angular.module("main_module", ["customer_module", "ngRoute", "authentication_module"]);
+	var ng = angular.module("main_module",["customer_module","ngRoute","authentication_module"]);
 
-    angular.module("main_module").config(function($routeProvider, $locationProvider) {
-        $routeProvider
-            .when("/login", {
+	ng.config(function($routeProvider, $locationProvider){
+		$routeProvider
+		.when("/login", {
                 'templateUrl': 'js/page/login.html',
                 'controller': function($scope, $location, AuthenticationService) {
                     $scope.login = function() {
@@ -21,34 +21,34 @@
                     };
                 }
             })
+		 .when("/", {
+			template : '<h1>Home Sweet Home!!!</h1>'
+		})
+		.when("/customers", {
+			templateUrl : 'js/page/customer.html'
+		})
+		.when("/orders", {
+			templateUrl : 'js/page/order.html',
+			controller: function($scope, CustomerService) {
+				CustomerService.getOrders().then(function(result) {
+					$scope.orders = result.data;
+				});
+			}
+		})
 
-        .when("/customers", {
-                'templateUrl': 'js/page/customers.html'
-            })
-            .when("/orders", {
-                'templateUrl': 'js/page/orders.html',
-                'controller': function($scope, CustomerService) {
-                    CustomerService.getOrders().then(function(result) {
-                        $scope.orders = result.data;
-                    });
-                }
-            })
-            .when("/", {
-                'templateUrl': 'js/page/home.html'
-            });
-
-        $locationProvider.html5Mode(true);
-    }).run(check); //config
+	}).run(check); //config
 
      function check($rootScope, $location, $cookieStore, $http) {
             $rootScope.globals = $cookieStore.get('globals') || {};
 
             $rootScope.$on('$locationChangeStart', function(event, next, current) {
-                var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+                var restrictedPage =
+                	 $.inArray($location.path(), ['/login', '/register']) === -1;
+
                 var loggedIn = $rootScope.globals.username;
                 if (restrictedPage && !loggedIn) {
                     $location.path('/login');
                 }
             });
-        }
+   }
 })();
